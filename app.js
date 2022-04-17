d25052f630330d043a1f01065b12a20eb36b294d
Jayu <jayeshhande00@gmail.com>
2022-04-17 11:35:10 +0530

    rename html

--- app.js
+++ app.js
@@ -5,46 +5,41 @@ const output = document.querySelector("#output");
 const author = document.querySelector("#auther");
 
 const quotes = [
-    {
-      quote:
-        "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
-      author: "__Life"
-    },
-    {
-      quote:
-        "Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all",
-      author: "__John Kenneth Galbraith"
-    },
-    {
-      quote:
-        "God save me from my friends. I can protect myself from my enemies.",
-      author: "__Claude Louis Hector de Villars "
-    },
-    {
-      quote: "The price of anything is the amount of life you exchange for it.",
-      author: "__David Thoreau"
-    },
-    {
-      quote:
-        "Life is like a landscape. You live in the midst of it but can describe it only from the vantage point of distance. ",
-      author: "__Charles Lindbergh"
-    },
-    {
-      quote:
-        "A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.",
-      author: "__Tyne Daly"
-    }
-  ];
+  {
+    quote:
+      "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
+    author: "__Life",
+  },
+  {
+    quote:
+      "Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all",
+    author: "__John Kenneth Galbraith",
+  },
+  {
+    quote: "God save me from my friends. I can protect myself from my enemies.",
+    author: "__Claude Louis Hector de Villars ",
+  },
+  {
+    quote: "The price of anything is the amount of life you exchange for it.",
+    author: "__David Thoreau",
+  },
+  {
+    quote:
+      "Life is like a landscape. You live in the midst of it but can describe it only from the vantage point of distance. ",
+    author: "__Charles Lindbergh",
+  },
+  {
+    quote:
+      "A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.",
+    author: "__Tyne Daly",
+  },
+];
 
+function generateQuates() {
+  let index = Math.floor(Math.random() * quotes.length);
+  output.innerText = quotes[index].quote;
 
-function generateQuates(){
-
-    let index = Math.floor(Math.random()*quotes.length);
-    output.innerText = quotes[index].quote; 
-
-    author.innerText = quotes[index].author
-
+  author.innerText = quotes[index].author;
 }
 
-
-button.addEventListener("click" , generateQuates);
+button.addEventListener("click", generateQuates);
